8a91f62d31ea58ccf2814ffcfe83456d00caf228
Praveen Elangovan <pelangovan@radiantblue.com>
2017-09-26 09:23:41 -0400

    uncommented out line

--- apps/omar-wms-app/grails-app/conf/logback.groovy
+++ apps/omar-wms-app/grails-app/conf/logback.groovy
@@ -20,5 +20,5 @@ if (Environment.isDevelopmentMode() && targetDir) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
- //   logger("omar.wms",INFO, ['STDOUT'],false)
+    logger("omar.wms",INFO, ['STDOUT'],false)
 }
