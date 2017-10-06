fdb7a7a26a46102ec3b431770c61d4827cc9736b
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2017-10-06 10:37:45 -0400

    revert bbox_midpoint to lat/lon style

--- plugins/omar-wms-plugin/grails-app/services/omar/wms/WebMappingService.groovy
+++ plugins/omar-wms-plugin/grails-app/services/omar/wms/WebMappingService.groovy
@@ -297,9 +297,7 @@ class WebMappingService implements InitializingBean
     omsParams.cutWmsBbox = "${bbox.minX},${bbox.minY},${bbox.maxX},${bbox.maxY}"
     omsParams.srs = bbox?.proj.id
     
-    def bbox_midy = (bbox.minY + bbox.maxY) / 2
-    def bbox_midx = (bbox.minX + bbox.maxX) / 2
-    bbox_midpoint = [ bbox_midx, bbox_midy ]
+    bbox_midpoint = [ lat: (bbox.minY + bbox.maxY) / 2, lon: (bbox.minX + bbox.maxX) / 2 ]
 
     def result = callOmsService( omsParams )
 
