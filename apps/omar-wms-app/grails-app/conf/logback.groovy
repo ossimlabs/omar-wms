76a9136314181bd3664eee023af3d4ac23b9977c
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2017-10-11 13:12:55 -0400

    add sleuth and stdout logger

--- apps/omar-wms-app/grails-app/conf/logback.groovy
+++ apps/omar-wms-app/grails-app/conf/logback.groovy
@@ -4,14 +4,20 @@ import grails.util.Environment
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        //pattern = "%level %logger - %msg%n"
-        pattern = "%5p [%logger,%X{X-B3-TraceId:-},%X{X-B3-SpanId:-},%X{X-Span-Export:-}] %msg%n"
+        pattern = "%msg%n"
     }
 }
 
-root(ERROR, ['STDOUT'])
-logger("omar.wms",INFO, ['STDOUT'],false)
+appender('SLEUTH', ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} %5p [%logger,%X{X-B3-TraceId:-},%X{X-B3-SpanId:-},%X{X-Span-Export:-}] %msg%n"
+    }
+}
 
+
+root(ERROR, ['STDOUT'])
+logger("omar.wms",INFO, ['SLEUTH'],false)
+logger("omar.wms",INFO, ['STDOUT'],false)
 
 def targetDir = BuildSettings.TARGET_DIR
 if (Environment.isDevelopmentMode() && targetDir) {
