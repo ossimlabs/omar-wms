e361b57eea072b0700835d29216b5844ee933bc6
Garrett Potts <gpotts@radiantblue.com>
2017-10-15 21:19:25 -0400

    Addeding swagger modifications

--- plugins/omar-wms-plugin/grails-app/controllers/omar/wms/WmsController.groovy
+++ plugins/omar-wms-plugin/grails-app/controllers/omar/wms/WmsController.groovy
@@ -48,7 +48,9 @@ class WmsController
 	 *
 	 * @param  wmsParams parameters to the WMS service request GetCapabilities
 	 */
-	@ApiOperation( value = "Get the capabilities of the server", produces = 'application/vnd.ogc.wms_xml' )
+	@ApiOperation( value = "Get the capabilities of the server", 
+                  produces = 'application/vnd.ogc.wms_xml', 
+                  httpMethod = "GET")
 	@ApiImplicitParams( [
 			@ApiImplicitParam( name = 'service', value = 'OGC Service type', allowableValues = "[WMS]", defaultValue = 'WMS', paramType = 'query', dataType = 'string', required = true ),
 			@ApiImplicitParam( name = 'version', value = 'Version to request', allowableValues = "[1.1.1, 1.3.0]", defaultValue = '1.3.0', paramType = 'query', dataType = 'string', required = true ),
@@ -72,54 +74,61 @@ class WmsController
 	 */
 	@ApiOperation( value = "Get image from the server",
 		produces = 'application/xml,application/json',
+      httpMethod="GET",
 		notes = """
-<style>
-    tab { padding-left: 2em; }
-</style>
-    <ul>
-        <li>
-            <b>version</b> can be 1.1.1 or 1.3.0.  If 1.3.0 is used <b>crs</b> must be specified and if version is 1.1.1 is used then <b>srs</b> field is used<br/>
-            <b>request</b> must be GetMap<br/>
-            <b>layers</b> can be either of the form omar:raster_entry or omar:raster_entry.id where <b>id</b> is the record ID in the database<p/>
-            <b>filter</b> can contain the where clause of the table we are querying.<p/>
-            <b>srs</b> is the spatial reference system of the form EPSG:code where <b>code</b> is a spatial reference code such as 4326 or 3857, .. etc<p/>
-            <b>crs</b> is the spatial reference system of the form EPSG:code where <b>code</b> is a spatial reference code such as 4326 or 3857, .. etc<p/>
-            <b>bbox</b> cut box in the units of the srs or crs code.  Is comma separated values of the form minx,miny,maxx,maxy <p/>
-            <b>width</b> defines the pixel width of the <b>bbox</b> cut.<p/>
-            <b>height</b> defines the pixel height of the <b>bbox</b> cut<p/>
-            <b>exceptions</b> defines the type of excpetions you can return. values can be one of application/vnd.ogc.se_xml,application/vnd.ogc.se_inimage,application/vnd.ogc.se_blank<p/>
-            <b>styles</b><p/>
-            Is a JSON formated string. That allows one to have added control over the pixel return of the image(s). Here is an example call:
-            <pre>
-     {"bands": "1,2,3",
-     "histOp": "auto-percentile",
-     "sharpenMode" : "none",
-     "contrast" : "1.0",
-     "brightness" : "0.0",
-     "resamplerFilter": "bilinear",
-     "histCenterTile": "false"
-     }
-            </pre>
-            where <br/>
-            <ul>
-	            <tab><b>bands:</b> is a one based band selection list.  First band starts at 1.</tab><br/>
-	            <tab><b>histOp:</b> values supported none, auto-minmax, </tab><br/>
-	            <tab>auto-percentile, std-stretch-1, std-stretch-2, or std-stretch-3</tab><br/>
-	            <tab><b>sharpenMode:</b> values supported  none, light, or heavy.</tab><br/>
-	            <tab><b>contrast:</b> Allows one to control the contrast of an<br/>
-	            <tab>image. This is a multiplier.</tab><br/>
-	            <tab><b>brightness:</b> Allows one to control the brightness of the image.<br/>
-	            <tab>This is expressed as a normalized value between -1 and 1.<br/>
-	            <tab>You can go higher values but just know it's a normalized and not absolute values.</tab><br/>
-	            <tab><b>resamplerFilter:</b>values supported nearest-neighbor, bilinear, </br>
-	            <tab>cubic, gaussian, blackman, bspline, hanning, hamming, hermite, mitchell, quadratic,<br/>
-	            <tab>sinc, magic</tab><br/>
-               <tab><b>histCenterTile:</b>Currently calculates the histogram from center of image.  Can be true|false<br/>
-            </ul>
-        </li>
-        <br>
-     </ul>
-		"""
+* **version** can be 1.1.1 or 1.3.0. If 1.3.0 is used **crs** 
+   must be specified and if version is 1.1.1 is used then **srs** 
+   field is used  
+* **request** must be GetMap  
+* **layers** can be either of the form omar:raster_entry or 
+   omar:raster_entry.id where **id** is the record ID in the database
+* **filter** can contain the where clause of the table we are 
+   querying.
+* **srs** is the spatial reference system of the form EPSG:code 
+   where **code** is a spatial reference code such as 4326 or 3857, 
+   .. etc
+* **crs** is the spatial reference system of the form EPSG:code 
+   where **code** is a spatial reference code such as 4326 or 3857, 
+   .. etc
+* **bbox** cut box in the units of the srs or crs code. Is comma 
+   separated values of the form minx,miny,maxx,maxy
+* **width** defines the pixel width of the **bbox** cut.
+* **height** defines the pixel height of the **bbox** cut
+* **exceptions** defines the type of excpetions you can return. 
+   values can be one of application/vnd.ogc.se_xml,application/vnd.ogc.se_inimage,application/vnd.ogc.se_blank
+* **styles** Is a JSON formated string. That allows one to have 
+   added control over the pixel return of the image(s). Here is an 
+   example call:
+```
+   {
+      "bands": "1,2,3",
+      "histOp": "auto-percentile",
+      "sharpenMode" : "none",
+      "contrast" : "1.0",
+      "brightness" : "0.0",
+      "resamplerFilter": "bilinear",
+      "histCenterTile": "false"
+   }
+```
+where: 
+
+* **bands:** is a one based band selection list. First band starts at 1.
+
+* **histOp:** values supported none, auto-minmax,
+   auto-percentile, std-stretch-1, std-stretch-2, or std-stretch-3
+
+* **sharpenMode:** values supported none, light, or heavy.
+
+* **contrast:** Allows one to control the contrast of an  
+   image. This is a multiplier. 
+* **brightness:** Allows one to control the brightness of the image.  
+    This is expressed as a normalized value between -1 and 1.  
+    You can go higher values but just know it's a normalized and not absolute values.
+* **resamplerFilter:** values supported nearest-neighbor, bilinear,  
+    cubic, gaussian, blackman, bspline, hanning, hamming, hermite, mitchell, quadratic,  
+    sinc, magic  
+* **histCenterTile:**Currently calculates the histogram from center of image. Can be true|false  
+    """
 		 )
 	@ApiImplicitParams( [
 			@ApiImplicitParam( name = 'service', value = 'OGC service type', allowableValues = "[WMS]", defaultValue = 'WMS', paramType = 'query', dataType = 'string', required = true ),
