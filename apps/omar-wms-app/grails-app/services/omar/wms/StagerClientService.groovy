b14e93c0e92f26f3ad61b0868c15f5b080f73792
arocnies <arocnies@gmail.com>
2018-05-23 20:23:40 -0400

    Debugging

--- apps/omar-wms-app/grails-app/services/omar/wms/StagerClientService.groovy
+++ apps/omar-wms-app/grails-app/services/omar/wms/StagerClientService.groovy
@@ -2,18 +2,19 @@ package omar.wms
 
 import grails.transaction.Transactional
 import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
 import org.springframework.beans.factory.annotation.Value
 
-@Transactional (readOnly = true)
+@Transactional(readOnly = true)
 class StagerClientService {
-    @Value ('${omar.wms.stager.url}')
+    @Value('${omar.wms.stager.url}')
     String stagerEndpoint
 
     def updateLastAccessDates(List<String> rasterEntryIds) {
-        String idCsv = rasterEntryIds.join(",")
-        def url = "${stagerEndpoint}/updateAccessDates?rasterEntries=$idCsv".toURL()
-        println "DEBUG: URL results = $url"
-        new JsonSlurper().parse(url)
+        if (!rasterEntryIds.isEmpty()) {
+            String idCsv = rasterEntryIds.join(",")
+            def url = "${stagerEndpoint}/updateAccessDates?rasterEntries=$idCsv".toURL()
+            println "DEBUG: URL results = $url"
+            new JsonSlurper().parse(url)
+        }
     }
 }
