01b629b638be9648dfdeef2c437996cc1671f7f7
Cory Robertson <coryrobertson@Corys-MacBook-Pro.local>
2020-11-09 14:31:47 -0500

    even more bug fixes

--- plugins/omar-wms-plugin/src/main/groovy/omar/wms/plugin/OmarWmsPluginGrailsPlugin.groovy
+++ plugins/omar-wms-plugin/src/main/groovy/omar/wms/plugin/OmarWmsPluginGrailsPlugin.groovy
@@ -53,18 +53,18 @@ Brief summary/description of the plugin.
         // TODO Implement post initialization spring config (optional)
     }
 
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+//    void onChange(Map<String, Object> event) {
+//        // TODO Implement code that is executed when any artefact that this plugin is
+//        // watching is modified and reloaded. The event contains: event.source,
+//        // event.application, event.manager, event.ctx, and event.plugin.
+//    }
+//
+//    void onConfigChange(Map<String, Object> event) {
+//        // TODO Implement code that is executed when the project configuration changes.
+//        // The event is the same as for 'onChange'.
+//    }
+//
+//    void onShutdown(Map<String, Object> event) {
+//        // TODO Implement code that is executed when the application shuts down (optional)
+//    }
 }
