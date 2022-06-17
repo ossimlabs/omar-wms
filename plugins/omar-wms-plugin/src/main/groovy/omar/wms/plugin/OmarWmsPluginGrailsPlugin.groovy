fcad8b3ec41c910c4ccfa0d3e45e4df33605e9b1
sbortman <scott.bortman@maxar.com>
2022-06-17 08:08:51 -0400

    hackaton-1

--- plugins/omar-wms-plugin/src/main/groovy/omar/wms/plugin/OmarWmsPluginGrailsPlugin.groovy
+++ plugins/omar-wms-plugin/src/main/groovy/omar/wms/plugin/OmarWmsPluginGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class OmarWmsPluginGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.0 > *"
+    def grailsVersion = "5.1.7 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
@@ -45,26 +45,26 @@ Brief summary/description of the plugin.
         }
     }
 
-//    void doWithDynamicMethods() {
-//        // TODO Implement registering dynamic methods to classes (optional)
-//    }
-//
-//    void doWithApplicationContext() {
-//        // TODO Implement post initialization spring config (optional)
-//    }
+    void doWithDynamicMethods() {
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
 
-//    void onChange(Map<String, Object> event) {
-//        // TODO Implement code that is executed when any artefact that this plugin is
-//        // watching is modified and reloaded. The event contains: event.source,
-//        // event.application, event.manager, event.ctx, and event.plugin.
-//    }
-//
-//    void onConfigChange(Map<String, Object> event) {
-//        // TODO Implement code that is executed when the project configuration changes.
-//        // The event is the same as for 'onChange'.
-//    }
-//
-//    void onShutdown(Map<String, Object> event) {
-//        // TODO Implement code that is executed when the application shuts down (optional)
-//    }
+    void doWithApplicationContext() {
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    void onChange(Map<String, Object> event) {
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    void onConfigChange(Map<String, Object> event) {
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    void onShutdown(Map<String, Object> event) {
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
 }
